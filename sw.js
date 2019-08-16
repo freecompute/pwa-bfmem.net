fe8c922fa0d543b282f99544c43ec087cb77eea2
freecompute <dan2yun@hotmail.com>
2019-08-16 23:31:23 +0800

    add debug info

--- sw.js
+++ sw.js
@@ -1,13 +1,35 @@
+var cname = 'bfmem.net.0.1'
 function installcb(e){
-	e.waitUntil(caches.open('bfmem.net.0.1.0').then(function(cache){
+	e.waitUntil(caches.open(cname).then(function(cache){
+		console.log('open ... and add assets')
 		return cache.add('index.html')
-	}))
+	}).catch(err){
+		console.log('open err : '+err)
+	})
 }
 self.addEventListener('install',installcb)
 
 function fetchcb(e){
 	e.respondWith(caches.match(e.request).then(function(res){
-		return res || fetch(e.request)
+		if(res != undefined){
+			return res
+		}else{
+			return fetch(e.request).then(function(r){
+				var rc = r.clone()
+				caches.open(cname).then(
+					function(c){
+						console.log('fetch ... open')
+						c.put(e.request , rc)
+					}
+				)
+				return r
+			}
+			).catch(function(){
+				console.log('fetch ...')
+			}
+			)
+		}
+		//return res || fetch(e.request)
 	}))
 }
 self.addEventListener('fetch' , fetchcb)
